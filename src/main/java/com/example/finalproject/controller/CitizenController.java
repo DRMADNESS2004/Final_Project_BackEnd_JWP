eb699dcde4de1117e376506ec71d774da05a718e
Caleb <liucaleb8@gmail.com>
2022-12-23 01:07:14 -0500

    delete + update citizen

---
+++ src/main/java/com/example/finalproject/controller/CitizenController.java
@@ -0,0 +1,26 @@
+package com.example.finalproject.controller;
+
+import com.example.finalproject.request.CitizenRequest;
+import com.example.finalproject.response.CitizenResponse;
+import com.example.finalproject.service.CitizenService;
+import com.example.finalproject.service.CountryService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/citizens")
+public class CitizenController {
+    @Autowired
+    private CitizenService citizenService;
+
+    @DeleteMapping("/{citizenId}")
+    public void deleteCitizen(@PathVariable long citizenId){
+        citizenService.deleteCitizen(citizenId);
+    }
+
+    @PatchMapping("/{citizenId}")
+    public CitizenResponse updateCitizen(@PathVariable long citizenId, @Valid @RequestBody CitizenRequest citizenRequest){
+        return new CitizenResponse(citizenService.updateCitizen(citizenId,citizenRequest));
+    }
+}
---
+++ src/main/java/com/example/finalproject/service/CitizenService.java
@@ -0,0 +1,34 @@
+package com.example.finalproject.service;
+
+import com.example.finalproject.entity.Citizen;
+import com.example.finalproject.exception.ResourceNotFoundException;
+import com.example.finalproject.repository.CitizenRepository;
+import com.example.finalproject.request.CitizenRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CitizenService {
+    @Autowired
+    private CitizenRepository citizenRepository;
+
+    public void deleteCitizen(long citizenId){
+        if(citizenRepository.existsById(citizenId)){
+            citizenRepository.deleteById(citizenId);
+        }
+        else{
+            throw new ResourceNotFoundException("citizen id was not found");
+        }
+    }
+
+    public Citizen updateCitizen(long citizenId, CitizenRequest citizenRequest){
+        if(citizenRepository.existsById(citizenId)){
+            Citizen citizenToBeUpdated=new Citizen(citizenRequest);
+            citizenToBeUpdated.setId(citizenId);
+            return citizenRepository.save(citizenToBeUpdated);
+        }
+        else{
+            throw new ResourceNotFoundException("citizen id not found");
+        }
+    }
+}
